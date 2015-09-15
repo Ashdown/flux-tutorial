d159f553732a2cca777bc0d56dfff7a61b26915c
Rory Devane <rory.devane@gmail.com>
2015-09-15 16:24:05 +0100

    added missing footer component

---
+++ js/components/Footer.react.js
@@ -0,0 +1,11 @@
+React = require('react');
+
+var Footer = React.createClass({
+    render: function() {
+        return(
+            <div>Footer Content goes here</div>
+        );
+    }
+});
+
+module.export = Footer;
